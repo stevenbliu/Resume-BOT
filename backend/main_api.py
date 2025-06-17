4f1d6822564689c5c4b374cc052da1aca67f3dd9
stevenbliu <steventheliu@gmail.com>
2025-06-17 08:07:06 -0700

    add endpoint to rag url

--- backend/main_api.py
+++ backend/main_api.py
@@ -67,35 +67,11 @@ async def answer(request: Request):
     question = data.get("question")
 
     async with httpx.AsyncClient() as client:
-        response = await client.post(RAG_SERVICE_URL, json={"question": question})
+        response = await client.post(
+            RAG_SERVICE_URL + "/answer", json={"question": question}
+        )
         # response = "dummy response"  # Replace with actual call to RAG service
         rag_response = response.json()
 
     return {"answer": rag_response.get("answer")}
     # return {"answer": response}
-
-
-@app.post("/wrong")
-async def wrong(request: Request):
-    print("Received request to /answer endpoint with body:", await request.body())
-    data = await request.json()
-    question = data.get("question")
-
-    async with httpx.AsyncClient() as client:
-        response = await client.post(RAG_SERVICE_URL, json={"question": question})
-        rag_response = response.json()
-
-    return {"answer": rag_response.get("answer")}
-
-
-@app.post("/asd")
-async def asd(request: Request):
-    print("Received request to /answer endpoint with body:", await request.body())
-    data = await request.json()
-    question = data.get("question")
-
-    async with httpx.AsyncClient() as client:
-        response = await client.post(RAG_SERVICE_URL, json={"question": question})
-        rag_response = response.json()
-
-    return {"answer": rag_response.get("answer")}
